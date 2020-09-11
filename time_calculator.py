ddf66c63eb497e4cc5cdb8d3cb82356259d3264c
Debojeet Das <debojeetofficial@gmail.com>
2020-09-11 16:05:45 +0000

    Conversion of start hours into minutes

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,15 @@
-def add_time(start, duration):
+def add_time(start, duration, dayOfWeek = None):
+  
+  #Conversion of Start 12 hour format to 24 hour format
+  startFormat = start.split(" ")
+  startTime = startFormat[0].split(":")
+  startAmPm = startFormat[1]
+  startHour = int(startTime[0])
+  startMinute = int(startTime[1])
+  
+  if startAmPm == "PM" :
+    startHour = startHour + 12
 
-
-
-
-
-    return new_time
+  #Conversion of Start hours into minutes
+  startMinute = startMinute + (60 * startHour)
+  return new_time
