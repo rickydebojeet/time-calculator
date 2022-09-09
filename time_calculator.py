7a4a593a34f6429c6ecdc1aa9e64089f6bc28937
cde200444 <112015216+cde200444@users.noreply.github.com>
2022-09-09 22:54:27 +0100

    Update time_calculator.py
    This is a 100% accurate version of the time calculator. The previous version did not give an accurate output for certain arguments

--- time_calculator.py
+++ time_calculator.py
@@ -1,69 +1,63 @@
-def add_time(start, duration, dayOfWeek = None):
-  
-  new_time = ""
+def add_time(start, duration, day = None):
+    duration_hour = int(duration[0:-3])
+    duration_minute = int(duration[-2:])
 
-  #Conversion of Start 12 hour format to 24 hour format
-  startFormat = start.split(" ")
-  startTime = startFormat[0].split(":")
-  startAmPm = startFormat[1]
-  startHour = int(startTime[0])
-  startMinute = int(startTime[1])
-  
-  if startAmPm == "PM" :
-    startHour = startHour + 12
+    if len(start) == 8:
+        start_hour=int(start[0:2])
+        start_minute=int(start[3:5])
 
-  #Conversion of Start hours into minutes
-  startMinute = startMinute + (60 * startHour)
+    elif len(start)==7:
+        start_hour=int(start[0:1])
+        start_minute=int(start[2:4])
 
-  #Conversion of duration hours into minutes
-  durationTime = duration.split(":")
-  durationHour = int(durationTime[0])
-  durationMinute = int(durationTime[1])
-  durationMinute = durationMinute + (60 * durationHour)
+    if start_hour==12:
+        start_hour=0
 
-  #Total minutes
-  minutes = startMinute + durationMinute
+    if start[-2:]=="PM":
+        start_hour+=12
+    minute=start_minute+duration_minute
 
-  #Minute calculation
-  finalMinutes = minutes % 60
-  hours = int(minutes / 60)
-  if len(str(finalMinutes)) == 1:
-    new_time = "0" + str(finalMinutes)
-  elif len(str(finalMinutes)) == 2:
-    new_time = str(finalMinutes)
+    if minute>=60:
+        minute-=60
+        duration_hour+=1
+    hour = start_hour+duration_hour
+    days = hour//24
+    day_list=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
-  #Days calculation
-  hour = hours % 24
-  days = int(hours / 24)
+    if day:
+        # The index() method returns the position at the first occurrence of the specified value.
+        index = day_list.index(day.capitalize())
+        index = (index+days) % 7
+        day = day_list[index]
+    hour = hour % 24
+    new_time = ''
 
-  #Hour and AM/PM calculation
-  finalHours = hour % 12
-  if int(hour / 12) == 0:
-    finalAmPm = 'AM'
-    if finalHours == 0:
-      finalHours = 12
-  else:
-    finalAmPm = 'PM'
-    if finalHours == 0:
-      finalHours = 12
-  new_time = str(finalHours) + ':' + new_time + ' ' + finalAmPm 
+    if hour>=12:
+        hour-=12
+        if hour==0:
+            hour=12
+        # The rjust() function takes two parameters. Eg; string.rjust(20, "o") adds 'o's before 
+        # the string and the number of 'o's plus the number of letters of the string adds up to 20
+        new_time+=str(hour)+':'+str(minute).rjust(2,'0')+' PM'
 
-  #Calculation of day of day Of Week
-  if not dayOfWeek == None:
-    daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    pos = 0
-    while True:
-      if dayOfWeek.lower() == daysOfWeek[pos].lower():
-        break
-      pos= pos + 1
-    newDayOfWeek = daysOfWeek[((pos + (days % 7)) % 7)]
-    new_time = new_time + ", " + newDayOfWeek
+    else:
+        if hour==0:
+            hour=12
+        new_time+=str(hour)+':'+str(minute).rjust(2,'0')+' AM'
+    if day:
+        new_time+=', ' + day
 
-  #Final output
-  if days == 1:
-    new_time = new_time + " (next day)"
-  elif days > 1:
-    days = str(days)
-    new_time = new_time + " (" + days + " days later)"
+    if days>0:
+        new_time+=' '
+        if days==1:
+            new_time+="(next day)"
+        else:
+            new_time+="({} days later)".format(days)
+    return new_time
 
-  return new_time
+print(add_time("5:01 AM", "0:00", "Monday"))
+print(add_time("12:00 AM", "5:00", "Tuesday"))
+print(add_time('11:59 PM', '48:01', "Wednesday"))
+print(add_time('12:00 AM', "12:00", "Monday"))
+print(add_time('12:00 PM', '25:00', 'Wednesday'))
+print(add_time('4:00 PM', "107:69", "Monday"))
